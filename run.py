74fc0088c2004d311a50780a5fbffb90411b5ffc
Rami <ramifareed1989@hotmail.com>
2020-01-21 17:04:33 +0000

    Final touches for the flask chat

--- run.py
+++ run.py
@@ -9,11 +9,9 @@ messages = []
 def add_messages(username, message):
     """Add messages to the 'messages' list"""
     now = datetime.now().strftime("%H:%M:%S")
-    messages.append("({}) {}: {}".format(now, username, message))
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(messages_dict)
 
-def get_all_messages():
-    """Get all of the messages and separate them with a 'br'"""
-    return "<br>".join(messages)
 
 @app.route('/', methods = ["GET", "POST"])
 def index():
@@ -27,10 +25,17 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/<username>')
+@app.route('/<username>', methods = ["GET", "POST"])
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_messages(username, message)
+        return redirect(session["username"])
+
+    return render_template("chat.html", username = username, chat_messages = messages)
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
